011478ca20fb40dc95e54cfcf8b1f56fcd3a08bb
mdaleardc <ismailaliamot@gmail.com>
2024-11-05 20:00:06 +0600

    Footer.jsx is updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,15 @@ import React from "react"
 const Footer = () => {
   
   return (
-    <>
-    <h1 className="bg-indigo-500 text-bold text-white">Footer Section</h1>
-    </>
+    <div>
+      <ul className="bg-red-600">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">AboutMe</a></li>
+        <li><a href="#">MyWorks</a></li>
+        <li><a href="#">MySkills</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
     )
 }
 
