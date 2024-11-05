d5c86ccbba99bc178efe1e649d4ff42ef52f0cd8
mdaleardc <ismailaliamot@gmail.com>
2024-11-05 20:06:22 +0600

    Footer.jsx is now up to date

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,13 +5,50 @@ const Footer = () => {
   
   return (
     <div>
-      <ul className="bg-red-600">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">AboutMe</a></li>
-        <li><a href="#">MyWorks</a></li>
-        <li><a href="#">MySkills</a></li>
-        <li><a href="#">Contact</a></li>
+      <div className="footer-div bg-[#111c] w-full text-[#fffc] grid grid-cols-2 md:grid-cols-4 items-start pl-[20px] pr-[20px] pt-[10px] pb-[10px]">
+    <div className="support-div">
+      <h1 className=" text-[#FF0078]">SUPPORT</h1>
+      <ul>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Contact us</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Promotions & Sale</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Track Order</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Privacy Policy</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Terms & Conditions</a></li>
+    </ul>
+    </div>
+    <div className="faq-div pt-[25px]">
+      <ul>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">FAQs</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">My Account</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Return Policy</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Tech Glossary</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Initial Return</a></li>
       </ul>
+    </div>
+    <div className="about-div justify-items-start">
+      <h1 className="text-[#FF0078]">ABOUT</h1>
+      <ul>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Company</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Corporate News</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Press Center</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Investors</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Careers</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Store Finder</a></li>
+      </ul>
+    </div>
+      <div className="social-media ">
+      <ul>
+        <h1 className="text-[#FF0078]">Stay Up to Date</h1>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Github</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">Codpen</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">X</a></li>
+        <li><a href="https://my-project-orpin-nine.vercel.app/">LinkedIn</a></li>
+      </ul>
+    </div>
+    </div>
+    <p className="text-right text-[#000c] pr-[17px] col-start-1 col-end-4 md:col-start-3 md:col-end-5">
+ Alright<sup>&copy;</sup> reserved  by Mohammed Ismail 2024
+    </p>
     </div>
     )
 }
