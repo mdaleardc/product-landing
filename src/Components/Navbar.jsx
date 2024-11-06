fe62fa06c195604aad282a4d39bd83fc9404cc91
mdaleardc <ismailaliamot@gmail.com>
2024-11-06 13:14:49 +0600

    updated Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
   return (
   <div className="py-[0.4rem] tx-[0.3rem] grid grid-cols-6 justify-center content-center">
     
-    <img src={weblogo} alt="logo-img" className="w-[50px] h-[50px] rounded-[50%] col-span-1"/>
+    <img src={weblogo} alt="logo-img" className="w-[50px] h-[50px] rounded-[50%] col-span-1 border-[3px] border-[#00F4FF] border-solid shadow-md shadow-[#00CDFF]"/>
     
-  <ul className="grid grid-cols-5 col-span-5 text-center justify-center items-center">
+  <ul className="grid grid-cols-5 col-span-5 text-center justify-center items-center text-[#FFFFFF] font-bold md:text-[2rem]">
     <li><a href="#home">Home</a></li>
     <li><a href="#aboutme">About<span className="text-[#FF0054]">Me</span></a></li>
-    <li><a href="#mywork">MyWork</a></li>
-    <li><a href="#myskills">MySkills</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#skills">Skills</a></li>
     <li><a href="#contact">Contact</a></li>
   </ul>
   </div>
