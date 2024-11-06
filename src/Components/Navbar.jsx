3560fffae1b9cca7c848ba07114ed630f83d8f30
mdaleardc <ismailaliamot@gmail.com>
2024-11-06 12:28:50 +0600

    Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,15 @@ import React from "react"
 import weblogo from "../Assets/weblogo.jpg"
 
 const Navbar = () => {
+  
   return (
-  <div className="py-[0.4rem] tx-[0.3rem] grid grid-cols-6 border-5 border-solid border-red-700">
+  <div className="py-[0.4rem] tx-[0.3rem] grid grid-cols-6 justify-center content-center">
     
-    <img src={weblogo} alt="logo-img" className="w-[50px] h-[50px] rounded-[50%] col-span-1 border-3 border-[green] border-solid"/>
+    <img src={weblogo} alt="logo-img" className="w-[50px] h-[50px] rounded-[50%] col-span-1"/>
     
-  <ul className="grid grid-cols-5 col-span-5 border-3 border-[red] border-solid">
+  <ul className="grid grid-cols-5 col-span-5 text-center justify-center items-center">
     <li><a href="#home">Home</a></li>
-    <li><a href="#aboutme">AboutMe</a></li>
+    <li><a href="#aboutme">About<span className="text-[#FF0054]">Me</span></a></li>
     <li><a href="#mywork">MyWork</a></li>
     <li><a href="#myskills">MySkills</a></li>
     <li><a href="#contact">Contact</a></li>
